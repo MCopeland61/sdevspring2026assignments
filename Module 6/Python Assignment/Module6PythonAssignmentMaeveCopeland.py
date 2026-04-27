3122fa090977ed11264b72de2b1d562efed4b7da
MCopeland61 <mcopeland61@ivytech.edu>
2026-04-26 21:46:04 -0400

    mod6pythonassignment

---
+++ Module 6/Python Assignment/Module6PythonAssignmentMaeveCopeland.py
@@ -0,0 +1,4 @@
+c = "cats" #string for cats
+d = "dogs" #string for dogs
+s = "It is raining " + c + " and " + d + "." #string that's made by concatenating the prior two strings into one sentence
+print(s) #prints our new sentence
